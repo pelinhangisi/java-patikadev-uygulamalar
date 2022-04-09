9a00167e09fbdc950b0a9f16577a659e0c118bc4
Pelin Hangisi <pelinhangisi@gmail.com>
2022-04-09 19:10:09 +0300

    Gelişmiş Hesap Makinesi

---
+++ Gelismis-Hesap-Mak/src/com/company/Main.java
@@ -0,0 +1,200 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void plus (){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Kaç adet sayı gireceksiniz :");
+        int counter = scan.nextInt();
+        int number, result = 0;
+
+        for (int i = 1; i <= counter; i++) {
+            System.out.print(i + ". sayı :");
+            number = scan.nextInt();
+            if (i == 1) {
+                result += number;
+                continue;
+            }
+            result += number;
+        }
+        System.out.println("Sonuç : " + result);
+    }
+
+    public static void minus() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Kaç adet sayı gireceksiniz :");
+        int counter = scan.nextInt();
+        int number, result = 0;
+
+        for (int i = 1; i <= counter; i++) {
+            System.out.print(i + ". sayı :");
+            number = scan.nextInt();
+            if (i == 1) {
+                result += number;
+                continue;
+            }
+            result -= number;
+        }
+
+        System.out.println("Sonuç : " + result);
+    }
+
+    public static void times() {
+        Scanner scan = new Scanner(System.in);
+        int number, result = 1, i = 1;
+
+        while (true) {
+            System.out.print(i++ + ". sayı :");
+            number = scan.nextInt();
+
+            if (number == 1)
+                break;
+
+            if (number == 0) {
+                result = 0;
+                break;
+            }
+            result *= number;
+        }
+
+        System.out.println("Sonuç : " + result);
+    }
+
+    public static void divided() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Kaç adet sayı gireceksiniz :");
+        int counter = scan.nextInt();
+        double number, result = 0.0;
+
+        for (int i = 1; i <= counter; i++) {
+            System.out.print(i + ". sayı :");
+            number = scan.nextDouble();
+            if (i != 1 && number == 0) {
+                System.out.println("Böleni 0 giremezsiniz.");
+                continue;
+            }
+            if (i == 1) {
+                result = number;
+                continue;
+            }
+            result /= number;
+        }
+
+        System.out.println("Sonuç : " + result);
+    }
+
+    public static void power() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Taban değeri giriniz :");
+        int base = scan.nextInt();
+        System.out.print("Üs değeri giriniz :");
+        int exponent = scan.nextInt();
+        int result = 1;
+
+        for (int i = 1; i <= exponent; i++) {
+            result *= base;
+        }
+
+        System.out.println("Sonuç : " + result);
+    }
+
+    public static void factorial() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Sayı giriniz :");
+        int n = scan.nextInt();
+        int result = 1;
+
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+
+        System.out.println("Sonuç : " + result);
+    }
+
+    public static void modAlma(){
+        Scanner scan = new Scanner (System.in);
+        System.out.print("Sayı giriniz : ");
+        int num= scan.nextInt();
+        System.out.print("Mod giriniz :");
+        int mod =scan.nextInt();
+        int result =0;
+        result = num%mod;
+
+        System.out.println("Sonuç :" + result);
+    }
+
+    public static void alanCevre(){
+        Scanner scan = new Scanner (System.in);
+        System.out.print("Kısa kenarı giriniz : ");
+        int kenar1=scan.nextInt();
+        System.out.print("Uzun kenarı giriniz : ");
+        int kenar2=scan.nextInt();
+        int cevre = 2*(kenar1+kenar2);
+        int alan = kenar1*kenar2;
+
+        System.out.println("Dikdörtgenin çevresi : " + cevre);
+        System.out.println("Dikdörtgenin alanı : " + alan);
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int select;
+        String menu = "1- Toplama İşlemi\n"
+                + "2- Çıkarma İşlemi\n"
+                + "3- Çarpma İşlemi\n"
+                + "4- Bölme işlemi\n"
+                + "5- Üslü Sayı Hesaplama\n"
+                + "6- Faktoriyel Hesaplama\n"
+                + "7- Mod Alma\n"
+                + "8- Dikdörtgen Alan ve Çevre Hesabı\n"
+                + "0- Çıkış Yap";
+
+        do {
+            System.out.println("");
+            System.out.println(menu);
+            System.out.print("Lütfen bir işlem seçiniz :");
+            select = scan.nextInt();
+            switch (select) {
+                case 1:
+                    plus();
+                    break;
+                case 2:
+                    minus();
+                    break;
+                case 3:
+                    times();
+                    break;
+                case 4:
+                    divided();
+                    break;
+                case 5:
+                    power();
+                    break;
+                case 6:
+                    factorial();
+                    break;
+                case 7:
+                    modAlma();
+                    break;
+                case 8 :
+                    alanCevre();
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Yanlış seçim yaptınız , tekrar deneyiniz.");
+            }
+        } while (select != 0);
+
+        System.out.println("Güle güle..");
+
+
+
+
+    }
+}
+
